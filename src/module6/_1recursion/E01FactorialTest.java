8b7ecf308a41d7c4c5b86925e277b7dbff1b3d8a
Ellie Ferguson <e.m.ferguson@wustl.edu>
2025-10-20 19:49:18 -0500

    done

--- src/module6/_1recursion/E01FactorialTest.java
+++ src/module6/_1recursion/E01FactorialTest.java
@@ -13,6 +13,28 @@ public class E01FactorialTest {
 			return n * factorial(n - 1);
 		}
 	}
+	//4! = 4*3*2*1 = 24
+	//n! = n * (n-1)!
+
+	public static int factorialLoop(int n){
+		int result = 1;
+		while(n != 0){
+			result = result * n;
+			n--;
+		}
+		return result;
+	}
+
+	// Substitution method
+	//factorial(4) = 4 * factorial(3)
+	//factorial(3) = 3 * factorial(2)
+	//factorial(2) = 2 * factorial(1)
+	//factorial(1) = 1 * factorial(0)
+	//factorial(0) = 1
+	//factorial(1) = 1 * 1 = 1
+	//factorial(2) = 2 * 1 = 2
+	//factorial(3) = 3 * 2 = 6
+	//factorial(4) = 4 * 6 = 24
 	
 	public static void main(String[] args) {
 		System.out.println(factorial(4));
