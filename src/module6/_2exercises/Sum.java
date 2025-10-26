fd86558637f54e748ca601bc891c460a8952f2bd
Ellie Ferguson <e.m.ferguson@wustl.edu>
2025-10-26 16:54:20 -0500

    done!

--- src/module6/_2exercises/Sum.java
+++ src/module6/_2exercises/Sum.java
@@ -1,8 +1,21 @@
 package module6._2exercises;
 
+import java.util.Scanner;
+
 public class Sum {
 
 	public static void main(String[] args) {
-		//test your method here
+		Scanner input = new Scanner(System.in);
+		System.out.println("What number do you want to sum?");
+		int sum = input.nextInt();
+		System.out.println(calculateSum(sum));
+		input.close();
+	}
+	public static int calculateSum(int n){
+		if (n == 0){
+			return 0;
+		} else {
+			return calculateSum(n-1)+n;
+		}
 	}
 }
